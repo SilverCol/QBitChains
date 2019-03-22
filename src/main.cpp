4f680684253c51c6f74c28b21ba7badfbf068d98
Mitja Vodnik <mitjavodnik@gmail.com>
2019-03-22 17:55:31 +0100

    Added gflags (also idea)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <gflags/gflags.h>
+
+DEFINE_int32(N, 20, "Number of qbits.");
+
+int main(int argc, char* argv[])
+{
+
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    std::cout << "Number of qbits: " << FLAGS_N << std::endl;
     return 0;
 }
